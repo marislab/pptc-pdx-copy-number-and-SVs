371b94ed446cb7b7f20a6a2c87592423eda50eca
Khushbu Patel <kpatel427@gatech.edu>
2019-03-14 16:26:05 -0400

    update

--- R/data-fetch-pptc-pdx-copy-number-SV.sh
+++ R/data-fetch-pptc-pdx-copy-number-SV.sh
@@ -16,8 +16,8 @@ cd ~
 mkdir -p copy-number-and-SVs/data/
 cd ~/copy-number-and-SVs/data/
 
-# 1. Download Expression matrix
-wget --output-document='2019-02-14-PPTC_FPKM_matrix_withModelID-244.rda' https://ndownloader.figshare.com/articles/7751825/versions/2
+# 1. Download Expression matrix2
+wget --output-document='2019-02-14-PPTC_FPKM_matrix_withModelID-244.rda' https://ndownloader.figshare.com/files/14452985
 
 # 2. Clinical file
 wget --output-document='pptc-pdx-clinical-web.txt' https://ndownloader.figshare.com/files/14508536
