e046592bedaa022c878254d0ecf7f0e2a3f3c8b2
Jo Lynne Rokita <jolynnerokita@d3b.center>
2019-12-27 13:05:20 -0500

    add bash script
    add bash script to change permissions of hg19.mat and run R script

---
+++ bash/run-CN-plots.sh
@@ -0,0 +1,8 @@
+###### create cohesive oncoprints to regenerate Figures 2 and 3########
+#
+#     Authors: Jo Lynne Rokita, Gonzalo Lopez     
+#     Updated 2019-12-27
+################################################################
+
+chmod 666 ~/pptc-pdx-copy-number-and-SVs/data/hg19.mat
+Rscript ~/pptc-pdx-copy-number-and-SVs/R/CN-plot-by-histology-revision.R
